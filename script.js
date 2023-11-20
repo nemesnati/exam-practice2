9a831c5fc72a0eb7a7d24e891e53a596b6734330
nemesnati <nemes.nati86@gmail.com>
2023-11-20 12:50:33 +0100

    ui build

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector("#root").insertAdjacentHTML(
+  "beforeend",
+  `
+    <form>
+      <h1>Register your pet!</h1>
+
+      <input type="text" placeholder="pet name">
+
+      <textarea placeholder="symptoms..."></textarea>
+
+      <button>submit</button>
+
+      <div>
+        <input type="checkbox">
+        <p>Elolvastem és elfogadom a felhasználási feltételeket</p>
+      </div>
+    </form>
+
+    <div>?</div>
+`
+);
